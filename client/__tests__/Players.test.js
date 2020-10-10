04df4e4ac6898ecbc41f4526fc83b86a4608fa1a
Nigel Reoch <nreoch9@gmail.com>
2020-10-10 17:13:49 -0400

    Added tests for pagination

--- client/__tests__/Players.test.js
+++ client/__tests__/Players.test.js
@@ -5,32 +5,13 @@ import toJson from "enzyme-to-json";
 import { act } from "react-dom/test-utils";
 import Players from "../components/Players";
 import PLAYERS_QUERY from "../graphql/players.query";
+import { playersData } from "./mock/players";
 
 const playersMock = {
   request: { query: PLAYERS_QUERY },
   result: {
     data: {
-      players: [
-        {
-          _id: "5f81fc9cc8d1690bd9a0f15f",
-          team: "DAL",
-          name: "Ezekiel Elliott",
-          position: "RB",
-          attempts: 322,
-          attemptsPerGame: 21.5,
-          yards: "1,631",
-          averagePerCarry: 5.1,
-          yardsPerGame: 108.7,
-          td: 15,
-          longRush: "60T",
-          firstDowns: 91,
-          firstDownPercentage: 28.3,
-          twentyPlus: 14,
-          fortyPlus: 3,
-          fumbles: 5,
-          __typename: "Player",
-        },
-      ],
+      players: playersData,
     },
   },
 };
@@ -72,6 +53,43 @@ describe("Players", () => {
       wrapper.update();
     });
 
+    // console.log(wrapper.html());
+
     expect(wrapper.contains(<th>Ezekiel Elliott</th>)).toEqual(true);
+    expect(wrapper.contains(<th>Derrick Henry</th>)).toEqual(true);
+  });
+  it("should go to page 2 of pagination when Next button is clicked", async () => {
+    let wrapper;
+    await act(async () => {
+      // Mount the component
+      wrapper = mount(
+        <MockedProvider addTypename={false} mocks={[playersMock]} resolvers={{}}>
+          <Players />
+        </MockedProvider>
+      );
+
+      // Wait until the query is resolved
+      await wait(0);
+      wrapper.update();
+    });
+
+    let activeLink = wrapper.find("li.page-item.active");
+    // Starts on pagination page 1
+    expect(activeLink.contains(1)).toEqual(true);
+
+    const nextButton = wrapper.find('[aria-label="Next"]');
+    const previousButton = wrapper.find('[aria-label="Previous"]');
+
+    nextButton.simulate("click");
+
+    activeLink = wrapper.find("li.page-item.active");
+    // Clicking nextButton moves pagination to page 2
+    expect(activeLink.contains(2)).toEqual(true);
+
+    previousButton.simulate("click");
+
+    activeLink = wrapper.find("li.page-item.active");
+    // Clicking previousButton moves pagination back to page 1
+    expect(activeLink.contains(1)).toEqual(true);
   });
 });
